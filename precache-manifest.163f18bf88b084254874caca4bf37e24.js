39fd6af413a7c132c1d60cd9aebb7fbe71c46284
冯然峰 <fengranfeng@hnjing.com>
2019-06-15 15:59:44 +0800

    Updates

---
+++ precache-manifest.163f18bf88b084254874caca4bf37e24.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "911a9e8def3727bd9d98fc3a96a4aeae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "253f9f489588dc5cc6c9",
+    "url": "/static/css/2.6013250c.chunk.css"
+  },
+  {
+    "revision": "f1fab007c22b75dc8bd6",
+    "url": "/static/css/main.d811a366.chunk.css"
+  },
+  {
+    "revision": "253f9f489588dc5cc6c9",
+    "url": "/static/js/2.48cad2ea.chunk.js"
+  },
+  {
+    "revision": "f1fab007c22b75dc8bd6",
+    "url": "/static/js/main.6f6f3561.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9161e238b798c444b732dbbb7420a09e",
+    "url": "/static/media/404.9161e238.png"
+  },
+  {
+    "revision": "ab45504baf89be7c7792b4426c4a6594",
+    "url": "/static/media/bg.ab45504b.jpg"
+  },
+  {
+    "revision": "ba1f87ecbc18b2f0a8b6e29e8676ac82",
+    "url": "/static/media/logo.ba1f87ec.png"
+  }
+]);
